060a30a5df1de41abaa720bfb7606468833c8821
Allan <allanl_95@hotmail.com>
2020-07-31 16:11:57 -0700

    bug fix

--- src/components/AnimeVisualization/BarChart.js
+++ src/components/AnimeVisualization/BarChart.js
@@ -3,30 +3,6 @@ import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 
-// const data = [
-//   {
-//     name: 'Page A', pv: 2400, amt: 2400,
-//   },
-//   {
-//     name: 'Page B', pv: 1398, amt: 2210,
-//   },
-//   {
-//     name: 'Page C', pv: 9800, amt: 2290,
-//   },
-//   {
-//     name: 'Page D', pv: 3908, amt: 2000,
-//   },
-//   {
-//     name: 'Page E', pv: 4800, amt: 2181,
-//   },
-//   {
-//     name: 'Page F', pv: 3800, amt: 2500,
-//   },
-//   {
-//     name: 'Page G', pv: 4300, amt: 2100,
-//   },
-// ];
-
 const BarChartViz = ({data}) => {
 
     return (
